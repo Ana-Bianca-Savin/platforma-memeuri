0c845fb79ac29b5a9075c6a0404a4588299864c8
Ana-Bianca-Savin <biasavin13@gmail.com>
2022-12-01 00:25:12 +0200

    Made the navbar without working buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   const title = "MEME IT"
   return (
     <div className="App">
-      <div className="content">
+      <div className="container">
+        <Navbar />
         <h1>{ title }</h1>
+        test
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import MEMEIT from './MEMEITTEXT.png'
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar navbar-expand">
+            <div className="container">
+                <img src={ MEMEIT} alt="MEMEIT?" class="navbar-brand"/>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <button className="btn">Logare</button>
+                    </li>
+                    <li className="nav-item">
+                        <button className="btn">Creare cont</button>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
